59b95d1b69caf3184c8116ca9012457f4dc99b5a
kelj0 <kegljevickarlo@gmail.com>
2020-07-25 19:19:39 +0200

    implement http get

--- dr.buster.py
+++ dr.buster.py
@@ -1,4 +1,4 @@
-import socket, requests, itertools
+import socket, ssl, itertools
 from time import time
 from sys import exit
 from os.path import exists
@@ -10,11 +10,59 @@ WORDLIST_PATH = "./words.txt"
 FINDINGS = []
 URL = ""
 
+def get(url):
+    host = None
+    port = None
+    path = None
+    https = False
+    try:
+        if url.startswith("https"):
+            url = url.split("//")[1]
+            https = True
+        elif url.startswith("http"):
+            url = url.split("//")[1]
+        if ":" in url:
+            host, port_path = url.split(":")
+            port = int(port_path.split("/")[0])
+            path = "/".join(port_path.split("/")[1:])
+        else:
+            host = url.split("/")[0]
+            path = "/".join(url.split("/")[1:])
+    except Exception:
+        print("Cant parse url!")
+        exit(1)
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    response = None
+    if https:
+        s = ssl.wrap_socket(s, ssl_version=ssl.PROTOCOL_TLSv1)
+        port = port if port else 443
+    else:
+        port = port if port else 80
+    try:
+        s.connect((host, port))
+    except (socket.gaierror, TimeoutError):
+        print("Name or service not known!")
+        exit(1)
+    except ssl.SSLError:
+        print("%s doesnt seem to support TLSv1 (ELI5 dont use https in url!)" % (url, ))
+        exit(1)
+    except ConnectionRefusedError:
+        print("Host seems down..")
+        exit(1)
+
+    request = "GET /%s HTTP/1.1\r\nHost:%s\r\n\r\n" % (path,host)
+    s.send(request.encode())  
+    response = s.recv(12)
+    http_response = repr(response.decode())
+    #print(http_response.split())
+    http_response_len = len(http_response)
+    s.close() 
+
 def parse_url(url):
     print("Validating url %s" % (url, ))
-
+    
 def prepare_wordlists(path):
-    global WORD_LISTS, LOADED_WORDS
+    global WORD_LISTS
     lines = []
     print("Loading words from %s" % (path,))
     if exists(WORDLIST_PATH):
@@ -40,7 +88,7 @@ def scan_url(url, wordlist, process_id=None):
     if not url.endswith('/'):
         url+="/"
     for n, word in enumerate(wordlist):
-        requests.get(url + word) # IMPLEMENT WITH RAW SOCKETS
+        get(url + word)
         if process_id:
             print("PROCESS [%s] - scanning %s\r" % (process_id, url+word), end="")
         else:
@@ -68,6 +116,8 @@ if __name__ == '__main__':
     print("Starting Dr.buster..")
     URL = "http://localhost:5000/"
     parse_url(URL)
+    print("Initial GET to see if host is up") 
+    get(URL)
     prepare_wordlists(WORDLIST_PATH)
     start_scan(URL)
     generate_report()
